e128e925144cd7466a236a88b29460123904f54c
igor <igor.mtka@gmail.com>
2026-03-26 17:47:22 -0300

    adiciona get no webhook

--- app/routes/bot_webhook_routes.py
+++ app/routes/bot_webhook_routes.py
@@ -7,6 +7,18 @@ logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
+@router.get("/webhook/whatsapp")
+async def verify_webhook(request: Request):
+    params = request.query_params
+
+    mode = params.get("hub.mode")
+    token = params.get("hub.verify_token")
+    challenge = params.get("hub.challenge")
+
+    if mode == "subscribe" and token == "SEU_VERIFY_TOKEN":
+        return int(challenge)
+
+    return {"error": "Verification failed"}
 
 @router.post('/webhook/whatsapp')
 async def whatsapp_webhook(request: Request):
